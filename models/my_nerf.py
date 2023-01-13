2c5694d419c52384b468a6c6d317cc7a3e2bbefe
tangcy03 <tangcy03@outlook.com>
2023-01-13 17:30:13 +0800

    modify mcubes and add time_compare func

--- models/my_nerf.py
+++ models/my_nerf.py
@@ -5,13 +5,15 @@ import numpy as np
 from tqdm import tqdm
 import time
 
+
 class CheatNeRF():
     def __init__(self, nerf):
         super(CheatNeRF, self).__init__()
         self.nerf = nerf
-    
+
     def query(self, pts_xyz):
         return self.nerf(pts_xyz, torch.zeros_like(pts_xyz))
+
 
 class MyNeRF():
     def __init__(self):
@@ -22,26 +24,27 @@ class MyNeRF():
         self.N, _ = pts_xyz.shape
         self.N = round(self.N ** (1/3))
         # load from the disk if exists
-        try:
-            checkpoint = torch.load("checkpoints/sigma_color_save.pth")
-            self.volumes_sigma = checkpoint["volumes_sigma"]
-            self.volumes_color = checkpoint["volumes_color"]
-            assert self.volumes_sigma.shape == (self.N, self.N, self.N)
-            print('VALID CHECKPOINT FOUND', self.volumes_sigma.shape, self.volumes_color.shape)
-            return
-        except:
-            # 存储体积中各点的sigma和color
-            self.volumes_sigma = sigma.reshape((self.N, self.N, self.N))
-            self.volumes_color = color.reshape((self.N, self.N, self.N, 3))
+        # try:
+        #     checkpoint = torch.load("checkpoints/sigma_color_save.pth")
+        #     self.volumes_sigma = checkpoint["volumes_sigma"]
+        #     self.volumes_color = checkpoint["volumes_color"]
+        #     assert self.volumes_sigma.shape == (self.N, self.N, self.N)
+        #     print('VALID CHECKPOINT FOUND',
+        #           self.volumes_sigma.shape, self.volumes_color.shape)
+        #     return
+        # except:
+        # 存储体积中各点的sigma和color
+        self.volumes_sigma = sigma.reshape((self.N, self.N, self.N))
+        self.volumes_color = color.reshape((self.N, self.N, self.N, 3))
             # save to the disk
-            checkpoint = {
-                "volumes_sigma": self.volumes_sigma,
-                "volumes_color": self.volumes_color
-            }
-            torch.save(checkpoint, "checkpoints/sigma_color_save.pth")
-            print('NEW CHECKPOINT CREATED', self.volumes_sigma.shape, self.volumes_color.shape)
+            # checkpoint = {
+            #     "volumes_sigma": self.volumes_sigma,
+            #     "volumes_color": self.volumes_color
+            # }
+            # torch.save(checkpoint, "checkpoints/sigma_color_save.pth")
+            # print('NEW CHECKPOINT CREATED',
+            #       self.volumes_sigma.shape, self.volumes_color.shape)
 
-    
     def query(self, pts_xyz):
         # 获取带查询坐标的数量
         N_xyz, _ = pts_xyz.shape
@@ -49,13 +52,14 @@ class MyNeRF():
         sigma = torch.zeros(N_xyz, 1, device=pts_xyz.device)
         color = torch.zeros(N_xyz, 3, device=pts_xyz.device)
         # 获取待查询坐标的x,y,z分量
-        X_index = ((pts_xyz[:, 0] + 0.125) * 4 * self.N).clamp(0, self.N-1).long()
-        Y_index = ((pts_xyz[:, 1] - 0.75) * 4 * self.N).clamp(0, self.N-1).long()
-        Z_index = ((pts_xyz[:, 2] + 0.125) * 4 * self.N).clamp(0, self.N-1).long()
+        X_index = ((pts_xyz[:, 0] + 0.125) * 4 *
+                   self.N).clamp(0, self.N-1).long()
+        Y_index = ((pts_xyz[:, 1] - 0.75) * 4 *
+                   self.N).clamp(0, self.N-1).long()
+        Z_index = ((pts_xyz[:, 2] + 0.125) * 4 *
+                   self.N).clamp(0, self.N-1).long()
         # 根据索引的x,y,z坐标，获取对应的sigma和color
         sigma[:, 0] = self.volumes_sigma[X_index, Y_index, Z_index]
         color[:, :] = self.volumes_color[X_index, Y_index, Z_index]
 
         return sigma, color
-
-
--- run_nerf.py
+++ run_nerf.py
@@ -17,6 +17,7 @@ from models.my_nerf import MyNeRF, CheatNeRF
 from models.my_renderer import MyNerfRenderer
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+
 class Runner:
     def __init__(self, conf_path, mode='render', case='CASE_NAME', is_continue=False):
         self.device = torch.device('cpu')
@@ -30,7 +31,8 @@ class Runner:
         f.close()
 
         self.conf = ConfigFactory.parse_string(conf_text)
-        self.conf['dataset.data_dir'] = self.conf['dataset.data_dir'].replace('CASE_NAME', case)
+        self.conf['dataset.data_dir'] = self.conf['dataset.data_dir'].replace(
+            'CASE_NAME', case)
         self.base_exp_dir = self.conf['general.base_exp_dir']
         self.checkpoint_dir = self.conf['general.checkpoint_dir']
         os.makedirs(self.base_exp_dir, exist_ok=True)
@@ -44,20 +46,21 @@ class Runner:
         self.writer = None
 
         # Networks
-        self.coarse_nerf = NeRF(**self.conf['model.coarse_nerf']).to(self.device)
+        self.coarse_nerf = NeRF(
+            **self.conf['model.coarse_nerf']).to(self.device)
         self.fine_nerf = NeRF(**self.conf['model.fine_nerf']).to(self.device)
         self.my_nerf = MyNeRF()
         self.renderer = MyNerfRenderer(self.my_nerf,
-                                     **self.conf['model.nerf_renderer'])
+                                       **self.conf['model.nerf_renderer'])
         self.load_checkpoint(r'./nerf_model.pth', absolute=False)
-
 
     def load_checkpoint(self, checkpoint_name, absolute=False):
         if absolute:
             checkpoint = torch.load(checkpoint_name, map_location=self.device)
         else:
             # checkpoint = torch.load(os.path.join(self.base_exp_dir, 'checkpoints', checkpoint_name), map_location=self.device)
-            checkpoint = torch.load(os.path.join(self.checkpoint_dir, checkpoint_name), map_location=self.device)
+            checkpoint = torch.load(os.path.join(
+                self.checkpoint_dir, checkpoint_name), map_location=self.device)
         self.coarse_nerf.load_state_dict(checkpoint['coarse_nerf'])
         self.fine_nerf.load_state_dict(checkpoint['fine_nerf'])
         logging.info('End')
@@ -65,134 +68,251 @@ class Runner:
     def use_nerf(self):
         self.my_nerf = CheatNeRF(self.fine_nerf)
         self.renderer = MyNerfRenderer(self.my_nerf,
-                                     **self.conf['model.nerf_renderer'])
+                                       **self.conf['model.nerf_renderer'])
 
-    def save(self):
+    def save(self, timecompare_mode=False):
         RS = 128
         pts_xyz = torch.zeros((RS, RS, RS, 3))
-        for i in tqdm(range(RS)):
-            for j in range(RS):
-                pts_xyz[:, i, j, 0] = torch.linspace(-0.125, 0.125, RS)
-                pts_xyz[i, :, j, 1] = torch.linspace(0.75, 1.0, RS)
-                pts_xyz[i, j, :, 2] = torch.linspace(-0.125, 0.125, RS)
-        pts_xyz = pts_xyz.reshape((RS*RS*RS, 3))
-        batch_size = 1024
-        # load from the disk if exists
-        try:
-            checkpoint = torch.load("checkpoints/sigma_color.pth")
-            sigma = checkpoint["sigma"]
-            color = checkpoint["color"]
-            assert sigma.shape == (RS*RS*RS, 1)
-            print('CHECKPOINT LOADED, sigma ', sigma.shape, 'color', color.shape)
-        except:
+        if not timecompare_mode:
+            for i in tqdm(range(RS)):
+                for j in range(RS):
+                    pts_xyz[:, i, j, 0] = torch.linspace(-0.125, 0.125, RS)
+                    pts_xyz[i, :, j, 1] = torch.linspace(0.75, 1.0, RS)
+                    pts_xyz[i, j, :, 2] = torch.linspace(-0.125, 0.125, RS)
+            pts_xyz = pts_xyz.reshape((RS*RS*RS, 3))
+            batch_size = 1024
+            # load from the disk if exists
+            try:
+                checkpoint = torch.load("checkpoints/sigma_color.pth")
+                sigma = checkpoint["sigma"]
+                color = checkpoint["color"]
+                assert sigma.shape == (RS*RS*RS, 1)
+                print('CHECKPOINT LOADED, sigma ',
+                      sigma.shape, 'color', color.shape)
+            except:
+                sigma = torch.zeros((RS*RS*RS, 1))
+                color = torch.zeros((RS*RS*RS, 3))
+                for batch in tqdm(range(0, pts_xyz.shape[0], batch_size)):
+                    batch_pts_xyz = pts_xyz[batch:batch+batch_size]
+                    net_sigma, net_color = self.fine_nerf(
+                        batch_pts_xyz, torch.zeros_like(batch_pts_xyz))
+                    sigma[batch:batch+batch_size] = net_sigma
+                    color[batch:batch+batch_size] = net_color
+                # save to the disk
+                checkpoint = {
+                    "sigma": sigma,
+                    "color": color
+                }
+                torch.save(checkpoint, "checkpoints/sigma_color.pth")
+                print('NO VALID CHECKPOINT, GENERATED A NEW ONE')
+        else:
+            for i in range(RS):
+                for j in range(RS):
+                    pts_xyz[:, i, j, 0] = torch.linspace(-0.125, 0.125, RS)
+                    pts_xyz[i, :, j, 1] = torch.linspace(0.75, 1.0, RS)
+                    pts_xyz[i, j, :, 2] = torch.linspace(-0.125, 0.125, RS)
+            pts_xyz = pts_xyz.reshape((RS*RS*RS, 3))
+            batch_size = 1024
+
             sigma = torch.zeros((RS*RS*RS, 1))
             color = torch.zeros((RS*RS*RS, 3))
-            for batch in tqdm(range(0, pts_xyz.shape[0], batch_size)):
+            for batch in range(0, pts_xyz.shape[0], batch_size):
                 batch_pts_xyz = pts_xyz[batch:batch+batch_size]
-                net_sigma, net_color = self.fine_nerf(batch_pts_xyz, torch.zeros_like(batch_pts_xyz))
+                net_sigma, net_color = self.fine_nerf(
+                    batch_pts_xyz, torch.zeros_like(batch_pts_xyz))
                 sigma[batch:batch+batch_size] = net_sigma
                 color[batch:batch+batch_size] = net_color
-            # save to the disk
-            checkpoint = {
-                "sigma": sigma,
-                "color": color
-            }
-            torch.save(checkpoint, "checkpoints/sigma_color.pth")
-            print('NO VALID CHECKPOINT, GENERATED A NEW ONE')
 
         self.my_nerf.save(pts_xyz, sigma, color)
 
-    def render_video(self):
+    def render_video(self, timecompare_mode=False, filename='show'):
         images = []
         resolution_level = 1
         n_frames = 90
-        for idx in tqdm(range(n_frames)):
-            rays_o, rays_d = self.dataset.gen_rays_at(idx, resolution_level=resolution_level)
-            H, W, _ = rays_o.shape
-            rays_o = rays_o.reshape(-1, 3).split(1024)
-            rays_d = rays_d.reshape(-1, 3).split(1024)
+        if not timecompare_mode:
+            for idx in tqdm(range(n_frames)):
+                rays_o, rays_d = self.dataset.gen_rays_at(
+                    idx, resolution_level=resolution_level)
+                H, W, _ = rays_o.shape
+                rays_o = rays_o.reshape(-1, 3).split(1024)
+                rays_d = rays_d.reshape(-1, 3).split(1024)
 
-            out_rgb_fine = []
+                out_rgb_fine = []
 
-            for rays_o_batch, rays_d_batch in tqdm(zip(rays_o, rays_d)):
-                near, far = self.dataset.near_far_from_sphere(rays_o_batch, rays_d_batch)
-                background_rgb = torch.ones([1, 3], device=self.device) if self.use_white_bkgd else None
+                for rays_o_batch, rays_d_batch in tqdm(zip(rays_o, rays_d)):
+                    near, far = self.dataset.near_far_from_sphere(
+                        rays_o_batch, rays_d_batch)
+                    background_rgb = torch.ones(
+                        [1, 3], device=self.device) if self.use_white_bkgd else None
 
-                render_out = self.renderer.render(rays_o_batch,
-                                                rays_d_batch,
-                                                near,
-                                                far,
-                                                background_rgb=background_rgb)
+                    render_out = self.renderer.render(rays_o_batch,
+                                                      rays_d_batch,
+                                                      near,
+                                                      far,
+                                                      background_rgb=background_rgb)
 
-                def feasible(key): return (key in render_out) and (render_out[key] is not None)
+                    def feasible(key): return (key in render_out) and (
+                        render_out[key] is not None)
 
-                if feasible('fine_color'):
-                    out_rgb_fine.append(render_out['fine_color'].detach().cpu().numpy())
+                    if feasible('fine_color'):
+                        out_rgb_fine.append(
+                            render_out['fine_color'].detach().cpu().numpy())
 
-                del render_out
-            
-            img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape([H, W, 3]) * 256).clip(0, 255)
-            img_fine = cv.resize(cv.flip(img_fine, 0), (512, 512))
-            images.append(img_fine)
-            os.makedirs(os.path.join(self.base_exp_dir, 'render'), exist_ok=True)
-            cv.imwrite(os.path.join(self.base_exp_dir,  'render', '{}.jpg'.format(idx)), img_fine)
+                    del render_out
 
-        fourcc = cv.VideoWriter_fourcc(*'mp4v')
-        h, w, _ = images[0].shape
-        writer = cv.VideoWriter(os.path.join(self.base_exp_dir,  'render', 'show.mp4'),
-                                fourcc, 30, (w, h))
-        for image in tqdm(images):
-            image = image.astype(np.uint8)
-            writer.write(image)
-        writer.release()
+                img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape(
+                    [H, W, 3]) * 256).clip(0, 255)
+                img_fine = cv.resize(cv.flip(img_fine, 0), (512, 512))
+                images.append(img_fine)
+                os.makedirs(os.path.join(
+                    self.base_exp_dir, 'render'), exist_ok=True)
+                cv.imwrite(os.path.join(self.base_exp_dir,  'render',
+                                        '{}.jpg'.format(idx)), img_fine)
 
+            fourcc = cv.VideoWriter_fourcc(*'mp4v')
+            h, w, _ = images[0].shape
+            writer = cv.VideoWriter(os.path.join(self.base_exp_dir,  'render', 'show.mp4'),
+                                    fourcc, 30, (w, h))
+            for image in tqdm(images):
+                image = image.astype(np.uint8)
+                writer.write(image)
+            writer.release()
+        else:
+            for idx in range(n_frames):
+                rays_o, rays_d = self.dataset.gen_rays_at(
+                    idx, resolution_level=resolution_level)
+                H, W, _ = rays_o.shape
+                rays_o = rays_o.reshape(-1, 3).split(1024)
+                rays_d = rays_d.reshape(-1, 3).split(1024)
+
+                out_rgb_fine = []
+
+                for rays_o_batch, rays_d_batch in zip(rays_o, rays_d):
+                    near, far = self.dataset.near_far_from_sphere(
+                        rays_o_batch, rays_d_batch)
+                    background_rgb = torch.ones(
+                        [1, 3], device=self.device) if self.use_white_bkgd else None
+
+                    render_out = self.renderer.render(rays_o_batch,
+                                                      rays_d_batch,
+                                                      near,
+                                                      far,
+                                                      background_rgb=background_rgb)
+
+                    def feasible(key): return (key in render_out) and (
+                        render_out[key] is not None)
+
+                    if feasible('fine_color'):
+                        out_rgb_fine.append(
+                            render_out['fine_color'].detach().cpu().numpy())
+
+                    del render_out
+
+                img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape(
+                    [H, W, 3]) * 256).clip(0, 255)
+                img_fine = cv.resize(cv.flip(img_fine, 0), (512, 512))
+                images.append(img_fine)
+
+            fourcc = cv.VideoWriter_fourcc(*'mp4v')
+            h, w, _ = images[0].shape
+            writer = cv.VideoWriter(os.path.join(self.base_exp_dir,  'render_compare', '{}.mp4'.format(filename)),
+                                    fourcc, 30, (w, h))
+            for image in images:
+                image = image.astype(np.uint8)
+                writer.write(image)
+            writer.release()
 
     def render_image(self, idx=0):
         """
         Render an image at a given index in the dataset.
         """
         resolution_level = 1
-        rays_o, rays_d = self.dataset.gen_rays_at(idx, resolution_level=resolution_level)
+        rays_o, rays_d = self.dataset.gen_rays_at(
+            idx, resolution_level=resolution_level)
         H, W, _ = rays_o.shape
         rays_o = rays_o.reshape(-1, 3).split(1024)
         rays_d = rays_d.reshape(-1, 3).split(1024)
 
         out_rgb_fine = []
 
-        for rays_o_batch, rays_d_batch in tqdm(zip(rays_o, rays_d)):
-            near, far = self.dataset.near_far_from_sphere(rays_o_batch, rays_d_batch)
-            background_rgb = torch.ones([1, 3], device=self.device) if self.use_white_bkgd else None
+        for rays_o_batch, rays_d_batch in zip(rays_o, rays_d):
+            near, far = self.dataset.near_far_from_sphere(
+                rays_o_batch, rays_d_batch)
+            background_rgb = torch.ones(
+                [1, 3], device=self.device) if self.use_white_bkgd else None
 
             render_out = self.renderer.render(rays_o_batch,
-                                            rays_d_batch,
-                                            near,
-                                            far,
-                                            background_rgb=background_rgb)
+                                              rays_d_batch,
+                                              near,
+                                              far,
+                                              background_rgb=background_rgb)
 
-            def feasible(key): return (key in render_out) and (render_out[key] is not None)
+            def feasible(key): return (key in render_out) and (
+                render_out[key] is not None)
 
             if feasible('fine_color'):
-                out_rgb_fine.append(render_out['fine_color'].detach().cpu().numpy())
+                out_rgb_fine.append(
+                    render_out['fine_color'].detach().cpu().numpy())
 
             del render_out
-        
-        img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape([H, W, 3]) * 256).clip(0, 255)
+
+        img_fine = (np.concatenate(out_rgb_fine, axis=0).reshape(
+            [H, W, 3]) * 256).clip(0, 255)
         img_fine = cv.resize(cv.flip(img_fine, 0), (512, 512))
         os.makedirs(os.path.join(self.base_exp_dir, 'render'), exist_ok=True)
-        cv.imwrite(os.path.join(self.base_exp_dir,  'render', 'custom_angle', '{}.jpg'.format(idx)), img_fine)
+        cv.imwrite(os.path.join(self.base_exp_dir,  'render',
+                   'custom_angle', '{}.jpg'.format(idx)), img_fine)
+
+    def mcube_save(self, threshold=0.0):
+        """
+        Run marching cube algorithm and save it as obj file.
+        """
+        # Converting sigma to numpy array
+        sigma = self.my_nerf.volumes_sigma.detach().cpu().numpy()
+        # Marching cubes
+        vertices, triangles = mcubes.marching_cubes(sigma, threshold)
+        # Save mesh
+        os.makedirs(os.path.join(self.base_exp_dir, 'mesh'), exist_ok=True)
+        mcubes.export_obj(vertices, triangles, os.path.join(
+            self.base_exp_dir, 'mesh', 'mesh.obj'))
 
     def mcube(self, threshold=0.0):
         """
         Run marching cube algorithm to extract mesh from volume.
         """
+        # Converting sigma to numpy array
         sigma = self.my_nerf.volumes_sigma.detach().cpu().numpy()
         # Marching cubes
         vertices, triangles = mcubes.marching_cubes(sigma, threshold)
-
+        # Show mesh
+        mesh = trimesh.Trimesh(vertices / 128 - .5, triangles)
+        mesh.show()
         # Save mesh
         os.makedirs(os.path.join(self.base_exp_dir, 'mesh'), exist_ok=True)
-        mcubes.export_obj(vertices, triangles, os.path.join(self.base_exp_dir, 'mesh', 'mesh.obj'))
-        # mcubes.export_mesh(vertices, triangles, os.path.join(self.base_exp_dir, 'mesh', 'mesh.dae'))
+        mcubes.export_obj(vertices, triangles, os.path.join(
+            self.base_exp_dir, 'mesh', 'mesh.obj'))
+
+    def time_compare(self, idx=0, threshold=0.0):
+        """
+        Compare the time of rendering using different methods.
+        """
+        # Removing potential checkpoints
+        # if os.path.isfile("checkpoints/sigma_color.pth"):
+        #     os.remove("checkpoints/sigma_color.pth")
+        # if os.path.isfile("checkpoints/sigma_color_save.pth"):
+        #     os.remove("checkpoints/sigma_color_save.pth")
+        # Rendering using NeRF
+        start = time.time()
+        self.use_nerf()
+        self.render_video(timecompare_mode=True, filename='neural')
+        end = time.time()
+        print('Time of Nerual Rendering: {}s'.format(end - start))
+        # Rendering using Voxels
+        start = time.time()
+        self.save(timecompare_mode=True)
+        self.render_video(timecompare_mode=True, filename='voxel')
+        end = time.time()
+        print('Time of Voxel Rendering: {}s'.format(end - start))
 
 
 if __name__ == '__main__':
@@ -219,4 +339,9 @@ if __name__ == '__main__':
         runner.render_video()
     elif args.mode == 'mcube':
         runner.save()
+        runner.mcube_save(args.mcube_threshold)
+    elif args.mode == 'mcube_show':
+        runner.save()
         runner.mcube(args.mcube_threshold)
+    elif args.mode == 'time_compare':
+        runner.time_compare()
